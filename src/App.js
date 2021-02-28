9157f9d316f975b46d5a52f3a67f83a57081939b
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2021-02-28 15:20:43 +0000

    check

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Learndfds React 
         </a>
       </header>
     </div>
